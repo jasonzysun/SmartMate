3cb8506f08916da6e97039d1e749c32a63eda09d
jasonzysun <jasonzysun@163.com>
2025-03-08 17:48:04 +0800

    refactor(SmartMate):  Migrating the project from Vue 2 to Vue 3
    -Update the scripting language in App. vue to TypeScript
    -Modify the script reference in index.html to main.ts
    -Rewrite main.exe to main.ts and remove Vue 2 related code
    -Update the main entry in package.json to main.ts
    -Rename uni-promisify.adaptor.js to uni-promisify.adaptor.ts and update the code

--- App.vue
+++ App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import {onLaunch, onShow, onHide} from '@dcloudio/uni-app'
 onLaunch(() => {
   console.log('App Launch')
--- uni.promisify.adaptor.ts
+++ uni.promisify.adaptor.ts
@@ -1,12 +1,12 @@
 uni.addInterceptor({
-  returnValue (res) {
+  returnValue(res: any): any {
     if (!(!!res && (typeof res === "object" || typeof res === "function") && typeof res.then === "function")) {
       return res;
     }
     return new Promise((resolve, reject) => {
-      res.then((res) => {
-        if (!res) return resolve(res) 
-        return res[0] ? reject(res[0]) : resolve(res[1])
+      res.then((result: any) => {
+        if (!result) return resolve(result);
+        return result[0] ? reject(result[0]) : resolve(result[1]);
       });
     });
   },
