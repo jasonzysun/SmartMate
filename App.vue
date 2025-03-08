fc0d6e0cee42cb489b8bb3b39bc2ca91ab994f6c
jasonzysun <jasonzysun@163.com>
2025-03-08 17:47:02 +0800

    feat:  Initialize uni app project -Add. gitignore file, ignore unnecessary files and directories -Create an androidPrivacy. json file and set up an Android privacy prompt template -Modify the App. vue file and add comments for managing uni app pages -Update manifest. json file, configure basic application information and icons

--- App.vue
+++ App.vue
@@ -1,15 +1,20 @@
-<script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+<!-- App.vue -->
+<template>
+  <view class="app">
+    <!-- uni-app 的页面由 pages.json 管理，无需在 App.vue 中写路由视图 -->
+  </view>
+</template>
+
+<script script>
+const onLaunch = function() {
+	console.log('App Launch')
+}
+const onShow = function() {
+	console.log('App Show')
+}
+const onHide = function() {
+	console.log('App Hide')
+}
 </script>
 
 <style>
